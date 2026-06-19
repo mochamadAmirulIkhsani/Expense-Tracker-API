7d304a9ad02933822c90a3e0ae7dfec5d4dfc32e
Amirul Ikhsani <amirulikhsani41@gmail.com>
2026-06-19 20:19:24 +0700

    feat: implement authentication module with user registration and login functionality

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { JwtGuard } from './jwt/jwt.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() dto: RegisterDto) {
+  console.log('DTO:', dto);
+    return this.authService.register(dto.email, dto.password, dto.name);
+  }
+
+  @Post('login')
+  login(@Body() dto: LoginDto) {
+  console.log('DTO:', dto);
+    return this.authService.login(dto.email, dto.password);
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('me')
+  profile(@Req() req) {
+    return req.user;
+  }
+
+  @UseGuards(JwtGuard)
+  @Post('logout')
+  logout(@Req() req) {
+    return this.authService.logout();
+  }
+}
